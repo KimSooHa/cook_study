a2452872fa203736a74bd1f26d95589f6150d3a1
KimSooHa <tngk1001@naver.com>
2023-06-20 19:21:01 +0900

    [운영서버 관련 설정] 운영서버 관련 민감정보 외부로 빼기

--- src/main/java/com/study/cook/CookApplication.java
+++ src/main/java/com/study/cook/CookApplication.java
@@ -2,14 +2,20 @@ package com.study.cook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
+@EnableJpaAuditing // 엔티티 객체가 생성이 되거나 변경이 되었을 때 자동으로 값 등록
 @SpringBootApplication
 public class CookApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location=classpath:application.yml, /app/config/springboot-webservice/real-application.yml";
+	
 	public static void main(String[] args) {
-		SpringApplication.run(CookApplication.class, args);
+//		SpringApplication.run(CookApplication.class, args);
+		new SpringApplicationBuilder(CookApplication.class).properties(APPLICATION_LOCATIONS).run(args);
 	}
 
 	// put, delete 매핑 사용
