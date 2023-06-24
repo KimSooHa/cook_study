df938ac2cd379b2b70a6d44012713456b4246762
KimSooHa <tngk1001@naver.com>
2023-06-24 16:59:30 +0900

    [MemberServiceTest] 회원가입 test 구현

--- src/test/java/com/study/cook/service/MemberServiceTest.java
+++ src/test/java/com/study/cook/service/MemberServiceTest.java
@@ -1,18 +1,25 @@
 package com.study.cook.service;
 
+import com.study.cook.controller.MemberForm;
 import com.study.cook.domain.Member;
 import com.study.cook.dto.MemberLoginIdSearchCondition;
 import com.study.cook.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-//@Slf4j
+@Slf4j
 class MemberServiceTest {
 
     @Autowired
@@ -23,7 +30,8 @@ class MemberServiceTest {
     @Autowired
     EntityManager em;
 
-//    @BeforeEach
+    @BeforeEach
+    @Transactional
     public void testSave() {
         for (int i = 0; i < 20; i++) {
             memberRepository.save(new Member("member" + i, "member" + i, "member1234*", "member" + i + "@email.com", "010-1234-1234"));
@@ -41,10 +49,27 @@ class MemberServiceTest {
         Member findMember = memberService.findOne(condition);
 
         // then
-        Assertions.assertThat(findMember.getLoginId()).isEqualTo("member5");
+        assertThat(findMember.getLoginId()).isEqualTo("member5");
 //        log.info("memberLoginId = {}", findMember.getLoginId());
     }
 
+    @Test
+    @Transactional
+    @DisplayName("회원가입")
+    public void signupTest() {
+        // given
+        MemberForm form = new MemberForm();
+        form.setName("member");
+        form.setLoginId("member1010");
+        form.setPwd("member123**");
+        form.setEmail("membertest@naver.com");
+        form.setPhoneNum("010-0000-1111");
 
+        // when
+        Long joinedId = memberService.join(form);
+
+        // then
+        assertThat(memberService.findOneById(joinedId)).isEqualTo(memberService.findOneByEmail(form.getEmail()));
+    }
 
 }
