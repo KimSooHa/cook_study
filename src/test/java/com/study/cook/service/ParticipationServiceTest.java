9b1d034d15d282b4b105788d400842a8af96cbc2
KimSooHa <tngk1001@naver.com>
2023-07-27 23:30:27 +0900

    [ParticipationServiceTest] 참여자 등록 성공/실패 test 구현

--- src/test/java/com/study/cook/service/ParticipationServiceTest.java
+++ src/test/java/com/study/cook/service/ParticipationServiceTest.java
@@ -1,12 +1,12 @@
 package com.study.cook.service;
 
 import com.study.cook.domain.*;
+import com.study.cook.exception.ParticipateFailException;
 import com.study.cook.repository.CategoryRepository;
 import com.study.cook.repository.ClubRepository;
 import com.study.cook.repository.MemberRepository;
 import com.study.cook.repository.ParticipationRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,8 +17,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.study.cook.domain.ClubStatus.COMP;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
@@ -47,7 +48,35 @@ class ParticipationServiceTest {
     }
 
     @Test
-    void tryToCreate() {
+    @DisplayName("참여자 등록 성공")
+    void tryToCreate_success() {
+        // given
+        Club club = new Club("테스트용 쿡스터디 모집", "테스트", 5, "추후 공지");
+        Member member = memberRepository.findByLoginId("test1").get();
+        clubSave(club, member);
+
+        // when
+        Long participationId = participationService.tryToCreate(club.getId(), member);
+
+        // then
+        assertThat(clubRepository.findById(club.getId()).get().getParticipations().get(0).getId()).isEqualTo(participationId);
+    }
+
+    @Test
+    @DisplayName("참여자 등록 실패")
+    void tryToCreate_fail() {
+        // given
+        Club club = new Club("테스트용 쿡스터디 모집", "테스트", 2, "추후 공지");
+        List<Member> members = memberRepository.findAll();
+        clubSave(club, members.get(0));
+
+        // when
+        for (int i = 0; i < 2; i++)
+            participate(club, members.get(i));
+        club.setStatus(COMP);
+
+        // then
+        assertThatThrownBy(() -> participationService.tryToCreate(club.getId(), members.get(2))).isInstanceOf(ParticipateFailException.class);
     }
 
     @Test
